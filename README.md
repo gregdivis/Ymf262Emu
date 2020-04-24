8abf9b378b6af93404f60a2a2af92d386b6a31cf
gregdivis <64230965+gregdivis@users.noreply.github.com>
2020-04-24 17:57:52 -0400

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ymf262Emu
+
+This is a C# port of the [Java OPL3 emulator by Robson Cozendey](https://opl3.cozendey.com/).
+
+It has been modified for compatibility with .NET/C#, to remove a few lookup tables,
+and to make a few performance optimizations, but is otherwise structured the same as
+the original Java version.
---
+++ Ymf262Emu/Intrinsics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Ymf262Emu
+{
+    internal static class Intrinsics
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ExtractBits(uint value, byte start, byte length, uint mask)
+        {
+            if (System.Runtime.Intrinsics.X86.Bmi1.IsSupported)
+                return System.Runtime.Intrinsics.X86.Bmi1.BitFieldExtract(value, start, length);
+            else
+                return (value & mask) >> start;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Log2(double x) => Math.Log2(x);
+    }
+}
